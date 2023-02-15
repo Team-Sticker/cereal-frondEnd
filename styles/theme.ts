65603508dac156494721e3b9b099780cad6f54c1
BaeJoonSoo <baejoonsoo21@gmail.com>
2023-02-16 02:30:11 +0900

    style(#13): theme color setting

---
+++ styles/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  noRedColor: "#E41818",
+  mainColor: "#FFA620",
+  okeyGreen: "#13AE22",
+
+  whiteColor: "#FFFFFF",
+  blackColor: "#000000",
+
+  newGrayColor: "#F5F3F0",
+  lightGrayColor: "#D9D9D9",
+  thinGrayColor: "#8D8D8D",
+  dailyGrayColor: "#5F5F5F",
+  strongGrayColor: "#343A40",
+};
